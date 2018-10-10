df8bb91b0072c195f798fed06c29ccec30198fe8
sevenmoney <qiu756863752@163.com>
2018-10-11 00:14:11 +0800

    add
    add aspnetcoreSignalDemo

---
+++ signalRDemo/aspnetCore/SampleSignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SampleSignalRDemo.Hubs {
+    public class ChatHub:Hub {//中心控制器
+        /// <summary>
+        /// 服务器端发送消息
+        /// </summary>
+        /// <param name="message"></param>
+        [HubMethodName("SendMessage")]
+        public void SendMessage(string message) {
+            Clients.All.SendAsync("ReceiveMessage",message);//客户端接收消息方法
+        }
+    }
+}
---
+++ signalRDemo/aspnetCore/SampleSignalRDemo/wwwroot/js/chat.js
@@ -0,0 +1,18 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/ChatHub").build();//创建链接
+connection.on("ReceiveMessage", function (message) {//客户端ReceiveMessage方法 从中心接收消息并处理
+    var li = '<li>' + message + '</li>';
+    $("#messages").append(li);
+});
+connection.start().catch(function (err) {//开始链接
+//    return console.error(err.toString());//打印错误日志
+});
+$("#send").click(function () {//点击发送按钮
+    if ($("#sendMsg").val().trim() !== "") {
+        var message = $("#sendMsg").val().trim();
+        connection.invoke("SendMessage", message).then(result => {//回调函数
+            $("#sendMsg").val("");
+        }).catch(function (err) {//调用中心SendMessage方法
+//            return console.error(err.toString());//打印错误日志
+        });
+    }
+});
